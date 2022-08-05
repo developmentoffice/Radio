f63c9307450b8802249ca87acf6aa03ae6d3f915
Alex Novikov <aleksey.novikov@pimpay.ru>
2022-08-05 14:37:46 +0300

    Update statistics: add artists list

--- radio/scripts/setup.sh
+++ radio/scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update && apt upgrade -y
-apt install -y icecast2 libfdk-aac1 libsdl2-image-2.0-0 libsdl2-ttf-2.0-0 libsrt1 bubblewrap libao4 libavcodec58 libavdevice58 libavfilter7 libavformat58 libavutil56 libfaad2 libgd3 libjack-jackd2-0 liblo7 libmad0 libmagic1 libmp3lame0 libopus0 libportaudio2 libsamplerate0 libshine3 libsoundtouch1 libswresample3 libswscale5 libtag1v5 libvorbisfile3 libxpm4 logrotate ffmpeg mp3info
+apt install -y icecast2 libfdk-aac1 libsdl2-image-2.0-0 libsdl2-ttf-2.0-0 libsrt1 bubblewrap libao4 libavcodec58 libavdevice58 libavfilter7 libavformat58 libavutil56 libfaad2 libgd3 libjack-jackd2-0 liblo7 libmad0 libmagic1 libmp3lame0 libopus0 libportaudio2 libsamplerate0 libshine3 libsoundtouch1 libswresample3 libswscale5 libtag1v5 libvorbisfile3 libxpm4 logrotate ffmpeg vim
 apt install -y locales
 dpkg-reconfigure locales
 # ru_RU.UTF-8
--- radio/scripts/statistics.sh
+++ radio/scripts/statistics.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-total_seconds=$(( $(mp3info -p '%S + ' /radio/sounds/music/*.mp3) 0 ))
-printf 'Total playing time: %02dh:%02dm:%02ds\n' $((total_seconds/3600)) $((total_seconds%3600/60)) $((total_seconds%60))
+total_files=$(ls -la /radio/sounds/music/*.mp3 | wc -l)
+total_seconds=$(( $(mp3info -p "%S + " /radio/sounds/music/*.mp3) 0 ))
+
+printf "Total files: %d\n" $total_files
+printf "Total playing time: %02dh:%02dm:%02ds\n" $((total_seconds/3600)) $((total_seconds%3600/60)) $((total_seconds%60))
+
+echo "Generate artists list..."
+artists=()
+for file in /radio/sounds/music/*.mp3; do
+    artist=$(ffprobe "$file" 2>&1 | sed -E -n 's/^ *artist *: (.*)/\1/p')
+    if [[ ! "${artists[*]}" =~ "$artist" ]]; then
+        artists+=("$artist")
+    fi
+done
+printf "Artists (%d):\n" ${#artists[@]}
+printf "%s\n" "${artists[@]}" | sort
